dd03de24c59a28ae685799a683f60c04ab78a674
Jhon Magdalena <jfmv.vt@gmail.com>
2022-02-05 19:26:06 +0100

    FIX: proxies discarding 2

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const SocksAgent = require('axios-socks5-agent');
 const fs = require("fs");
 
 const proxies = {
+    good: {},
+    bad: {},
     // "socks5://192.168.0.13:9050": {
     //     key: "0",
     //     ip: '192.168.0.13',
@@ -41,7 +43,7 @@ async function getProxies() {
 
 
     await requestAndProcessPage('https://free-proxy-list.net/', {}, proxies, $ => $('#list > div > div.table-responsive > div > table > tbody > tr'),
-        function ($, listItem$, outResults, index) {
+        function ($, listItem$, proxies, index) {
 
             const ip$ = listItem$.find('td:nth-child(1)');
             const port$ = listItem$.find('td:nth-child(2)');
@@ -55,10 +57,16 @@ async function getProxies() {
                 let proxy = {
                     ip,
                     port,
-                    protocols: ['https']
+                    protocols: ['https'],
+                    lastCheck: Date.now()
                 };
                 proxy.key = `${proxy.protocols[0]}://${proxy.ip}:${proxy.port}`;
-                outResults[proxy.key] = proxy;
+                if (proxies.bad[proxy.key] && (proxies.bad[proxy.key].lastCheck || 0) < (Date.now() - 15000)) {
+                    delete proxies.bad[proxy.key]
+                }
+                if (!proxies.bad[proxy.key]) {
+                    proxies.good[proxy.key] = proxy;
+                }
             }
 
         }, reason => {
@@ -77,8 +85,9 @@ function saveProxies() {
     saving = false;
 }
 
+const gettingProxy = {};
 async function getNextProxy() {
-    if (!Object.keys(proxies).length) {
+    if (Object.keys(proxies.good).length < 4) {
         await getProxies();
     }
     let proxyAsSt;
@@ -86,41 +95,29 @@ async function getNextProxy() {
     let httpAgent, httpsAgent;
     let currentProxy;
     do {
-        if (!Object.keys(proxies).length) {
+        if (Object.keys(proxies.good).length < 4) {
             await getProxies();
         }
         currentProxy = getRandomProxyKey();
-        proxyAsSt = `${proxies[currentProxy].protocols[0]}://${proxies[currentProxy].ip}:${proxies[currentProxy].port}`;
+        while (gettingProxy[currentProxy]) {
+            await delay(67);
+        }
+        gettingProxy[currentProxy] = true;
+        if (!proxies.good[currentProxy]) {
+            error = true;
+            continue;
+        }
+        proxyAsSt = `${proxies.good[currentProxy].protocols[0]}://${proxies.good[currentProxy].ip}:${proxies.good[currentProxy].port}`;
 
-        ({httpAgent, httpsAgent} = SocksAgent({
-            agentOptions: {
-                keepAlive: true,
-            },
-            // socks5
-            host: proxies[currentProxy].ip,
-            port: proxies[currentProxy].port,
-            // socks5 auth
-            // username: 'admin',
-            // password: 'pass1234',
-        }));
-
-        if (!proxies[currentProxy].checked) {
-            console.log('Testing proxy', currentProxy, ", total =", Object.keys(proxies).length);
+        if (!proxies.good[currentProxy].checked) {
+            console.log('Testing proxy', currentProxy, ", total =", Object.keys(proxies.good).length);
 
             const response = await axios.get('https://api.ipify.org?format=json', {
-                // proxy: proxyAsSt,
                 proxy: {
-                    host: proxies[currentProxy].ip,
-                    port: proxies[currentProxy].port,
+                    host: proxies.good[currentProxy].ip,
+                    port: proxies.good[currentProxy].port,
                 },
-                // resolveWithFullResponse: true,
                 json: true,
-                // httpsAgent: new https.Agent({
-                //     rejectUnauthorized: false
-                // }),
-                // rejectUnauthorized:false,
-                // strictSSL: false,
-                // httpAgent, httpsAgent
                 timeout: 30000,
             }).catch(reason => {
                 console.log('PROXY ERROR: ', proxyAsSt, reason.name, reason.message);
@@ -129,23 +126,25 @@ async function getNextProxy() {
             });
             if (!response.data || !response.data.ip) {
                 error = true;
-                delete proxies[currentProxy];
+                proxies.bad[currentProxy] = proxies.good[currentProxy];
+                delete proxies.good[currentProxy];
             } else {
                 error = false;
-                proxies[currentProxy].checked = true;
+                proxies.good[currentProxy].checked = true;
                 console.log(`Proxy [${currentProxy}]${proxyAsSt} is OK`);
             }
             saveProxies();
         }
+        delete gettingProxy[currentProxy];
 
-    } while (error || !proxies[currentProxy]);
-    proxies[currentProxy].key = proxies[currentProxy].key || currentProxy
-    return {proxy: proxies[currentProxy], httpAgent, httpsAgent};
+    } while (error || !proxies.good[currentProxy]);
+    proxies.good[currentProxy].key = proxies.good[currentProxy].key || currentProxy
+    return {proxy: proxies.good[currentProxy], httpAgent, httpsAgent};
 }
 
 function getRandomProxyKey() {
-    let randomIndex = Math.floor(Math.random() * Object.keys(proxies).length);
-    return Object.keys(proxies)[randomIndex];
+    let randomIndex = Math.floor(Math.random() * Object.keys(proxies.good).length);
+    return Object.keys(proxies.good)[randomIndex];
 }
 
 async function requestAndProcessPage(url, options, outResults, listSelector, itemHandler, errorHandler, skipProxy = false) {
@@ -172,6 +171,7 @@ async function requestAndProcessPage(url, options, outResults, listSelector, ite
             // strictSSL: false,
             // resolveWithFullResponse: true,
             // httpAgent, httpsAgent,
+            timeout: 30000,
         }).catch(reason => {
             if (
                 reason.message.indexOf(':SSL ') !== -1 ||
@@ -183,12 +183,15 @@ async function requestAndProcessPage(url, options, outResults, listSelector, ite
                 (reason.response || {}).status === 403
             ) {
                 console.log('Discard proxy', proxy.key, 'for:', reason.message);
-                delete proxies[proxy.key];
+                proxies.bad[proxy.key] = proxy;
+                delete proxies.good[proxy.key];
                 saveProxies();
                 reTry = true;
             } else if (
                 reason.code === 'ERR_REQUEST_ABORTED' ||
-                (reason.response || {}).status === 500
+                (reason.response || {}).status === 500 ||
+                (reason.response || {}).status === 502 ||
+                (reason.response || {}).status === 504
             ) {
                 console.log('Failed with proxy', proxy.key, ', retry');
                 reTry = true;
